11bf64b0f7925ecaf27bf46e5e5934a7936fd9d7
dimedrollum <dimedrollum@DLL-Lenovo>
2013-04-17 15:47:06 +0200

    Check Login functionality completed

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -6,7 +6,27 @@ class Login_model extends CI_Model {
     
     public function correct_credentials()
     {
-	return "Дима";
+	//select db
+	//$this->load->database();
+	//set conditions
+	$this->db->where('login', $this->input->post('username'));
+	$this->db->where('password', $this->input->post('password'));
+	//run query
+	$query=$this->db->get('users');
+	$row = $query->row();
+	
+	
+
+	
+	if ($row)
+	{
+	
+	    return $row->name;
+	}
+	else
+	{
+	    return FALSE;
+	}
     }
     
 }
