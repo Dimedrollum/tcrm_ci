9f0ed162e37b14130cec2ad6128051afe9fce3ce
dimedrollum <dimedrollum@DLL-Lenovo>
2013-04-19 13:58:22 +0300

    DB updated. Activities and Programs added

--- tcrm.sql
+++ tcrm.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 04, 2013 at 07:58 PM
+-- Generation Time: Apr 19, 2013 at 12:56 PM
 -- Server version: 5.5.29-0ubuntu0.12.10.1
 -- PHP Version: 5.4.6-1ubuntu1.2
 
@@ -23,27 +23,63 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `activities`
+--
+
+CREATE TABLE IF NOT EXISTS `activities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) COLLATE utf8_bin NOT NULL,
+  `subtype` int(255) NOT NULL,
+  `date` date NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `contact` int(11) NOT NULL,
+  `comment` text COLLATE utf8_bin NOT NULL,
+  `created_on` date NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_on` date NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `people`
 --
 
 CREATE TABLE IF NOT EXISTS `people` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `last_name` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `patronymic` varchar(255) NOT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `first_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `patronymic` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `birthdate` date NOT NULL,
-  `sex` varchar(1) NOT NULL,
-  `home_phone` varchar(255) NOT NULL,
-  `mob_phone` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `town` varchar(255) NOT NULL,
+  `sex` varchar(1) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `home_phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `mob_phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `town` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `was_on_t` tinyint(1) NOT NULL,
   `was_on_m` tinyint(1) NOT NULL,
   `has_problems` tinyint(1) NOT NULL,
-  `proeblem_reason` varchar(255) NOT NULL,
+  `proeblem_reason` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id` (`id`,`last_name`,`first_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `programs`
+--
+
+CREATE TABLE IF NOT EXISTS `programs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(10) COLLATE utf8_bin NOT NULL,
+  `year` int(11) NOT NULL,
+  `subtype` varchar(255) COLLATE utf8_bin NOT NULL,
+  `status` varchar(10) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -53,9 +89,9 @@ CREATE TABLE IF NOT EXISTS `people` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(255) NOT NULL,
+  `login` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `password` varchar(255) NOT NULL,
-  `date` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `password_change` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`),
@@ -65,8 +101,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   KEY `id_3` (`id`),
   KEY `id_4` (`id`),
   KEY `id_5` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Dumping data for table `users`
+--
+
